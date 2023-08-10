4b30505c8b21a16dd0616dc38501ddfeb8cd89a5
cipher <asaforbrian18@gmail.com>
2023-08-11 02:54:48 +0300

    successfully uploaded a category to the firebase and cloud storage

--- multivendor_admin_pannel/lib/views/screens/side_bar_screens/categories_screen.dart
+++ multivendor_admin_pannel/lib/views/screens/side_bar_screens/categories_screen.dart
@@ -1,20 +1,167 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   static const String routeName = '\CategoriesScreen';
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  dynamic _image;
+
+  String? fileName;
+
+  late String categoryName;
+
+  _pickImage() async {
+    FilePickerResult? result = await FilePicker.platform
+        .pickFiles(allowMultiple: false, type: FileType.image);
+
+    if (result != null) {
+      setState(() {
+        _image = result.files.first.bytes;
+
+        fileName = result.files.first.name;
+      });
+    }
+  }
+
+  _uploadCategoryBannerToStorage(dynamic image) async {
+    //Create the folder and assign the file name
+    Reference ref = _storage.ref().child('categoryImages').child(fileName!);
+    //Push the data to the cloud storage
+    UploadTask uploadTask = ref.putData(image);
+    // wait for the data to be sent
+    TaskSnapshot snapshot = await uploadTask;
+    //fetch the download url of the image
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+
+    return downloadUrl;
+  }
+
+  uploadCategory() async {
+    EasyLoading.show();
+    if (_formKey.currentState!.validate()) {
+      String imageUrl = await _uploadCategoryBannerToStorage(_image);
+
+      await _firestore.collection('categories').doc(fileName).set({
+        'image': imageUrl,
+        'categoryName': categoryName,
+      }).whenComplete(() {
+        EasyLoading.dismiss();
+        setState(() {
+          _image = null;
+        });
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Category',
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 36,
-          ),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                'Categories',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 36,
+                ),
+              ),
+            ),
+            Divider(
+              color: Colors.grey,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 140,
+                        width: 140,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade500,
+                          border: Border.all(
+                            color: Colors.grey.shade900,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: _image != null
+                            ? Image.memory(
+                                _image,
+                                fit: BoxFit.cover,
+                              )
+                            : Center(
+                                child: Text("Category"),
+                              ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.green.shade800,
+                        ),
+                        onPressed: () {
+                          _pickImage();
+                        },
+                        child: Text("Upload Image"),
+                      ),
+                    ],
+                  ),
+                ),
+                Flexible(
+                  child: SizedBox(
+                    width: 180,
+                    child: TextFormField(
+                      onChanged: (value) {
+                        categoryName = value;
+                      },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Please Category Name must not be Empty';
+                        } else {
+                          return null;
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Enter Category Name',
+                        hintText: 'Enter Category Name',
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green.shade800,
+                  ),
+                  onPressed: () {
+                    uploadCategory();
+                  },
+                  child: Text("Save"),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
