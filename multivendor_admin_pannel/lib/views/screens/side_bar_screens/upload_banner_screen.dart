bdcbdab7fee993391566d7ac3af1f417bf8a84ea
cipher <asaforbrian18@gmail.com>
2023-08-10 22:38:17 +0300

    added a file pciker to the upload banner screen

--- multivendor_admin_pannel/lib/views/screens/side_bar_screens/upload_banner_screen.dart
+++ multivendor_admin_pannel/lib/views/screens/side_bar_screens/upload_banner_screen.dart
@@ -1,7 +1,26 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
-class UploadBannerScreen extends StatelessWidget {
+class UploadBannerScreen extends StatefulWidget {
   static const String routeName = '\UploadBannerScreen';
+
+  @override
+  State<UploadBannerScreen> createState() => _UploadBannerScreenState();
+}
+
+class _UploadBannerScreenState extends State<UploadBannerScreen> {
+  dynamic _image;
+
+  pickImage() async {
+    FilePickerResult? result = await FilePicker.platform
+        .pickFiles(allowMultiple: false, type: FileType.image);
+
+    if (result != null) {
+      setState(() {
+        _image = result.files.first.bytes;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +68,9 @@ class UploadBannerScreen extends StatelessWidget {
                       style: ElevatedButton.styleFrom(
                         primary: Colors.green.shade800,
                       ),
-                      onPressed: () {},
+                      onPressed: () {
+                        pickImage();
+                      },
                       child: Text("Upload Image"),
                     ),
                   ],
